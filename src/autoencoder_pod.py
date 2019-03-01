e5c8cb24b07da4053ef878483c2cb4671a3f8d18
yangzeng <jupiterzeng@hnu.edu.cn>
2019-03-01 11:05:20 -0500

    coding

--- src/autoencoder_pod.py
+++ src/autoencoder_pod.py
@@ -1,8 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 28 16:16:07 2019
+#!/usr/bin/env python
+# Copyright 2018 YangZeng
+""" Training the numeric data """
 
-@author: zengyang
-"""
+# standard library imports
+import os, time, sys
+import scipy.io as scio
+
+# third party imports
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+mat_file = scio.loadmat(sys.argv[1])
+
+parameters = mat_file['']
 
