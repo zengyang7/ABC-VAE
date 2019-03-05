2d3b6254c3f4a35616e611d13754b297eb0a65ab
yangzeng <jupiterzeng@hnu.edu.cn>
2019-03-04 20:22:12 -0500

    add qsub

---
+++ demo/6_nonlinear/cleanall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -rf seastate_with_wake 2> /dev/null
+rm -rf ss_* 2> /dev/null
+rm -rf distribution_data 2> /dev/null
+rm -rf Predicted_results 2> /dev/null
+rm -rf figs 2> /dev/null
---
+++ demo/6_nonlinear/train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#PBS -l procs=2,gpus=2
+#PBS -l walltime=01:00:00
+#PBS -q p100_normal_q
+#PBS -A mfulearn
+#PBS -W group_list=newriver
+#PBS -M yangzeng@vt.edu
+#PBS -m bea
+#PBS -N ABC_AE
+#PBS -j oe
+#PBS -o AE.log
+
+cd $PBS_O_WORKDIR
+
+module purge
+module load Anaconda/5.1.0
+module load cuda/9.0.176
+module load cudnn/7.1
+
+
+autoencoder_pod_w.py setting sensitive_data_6_10000.mat
